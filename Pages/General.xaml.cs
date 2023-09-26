2958e179408fa971918d043e3187af0897aec026
JT Tognucci <jtognucc@villanova.edu>
2023-09-26 09:34:01 -0400

    organized general

--- Pages/General.xaml.cs
+++ Pages/General.xaml.cs
@@ -1,9 +1,9 @@
-namespace MauiApp1.Pages;
+namespace MauiApp1;
 
 public partial class General : ContentPage
 {
 	public General()
 	{
-		InitializeComponent();
+       InitializeComponent();
 	}
 }
