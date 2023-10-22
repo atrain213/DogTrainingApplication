60faf1b3a20d809143694cff45e3ad65ed17172a
JT Tognucci <jtognucc@villanova.edu>
2023-10-22 14:04:13 -0400

    changed list view of general page, added general model to base it off of

---
+++ GeneralModel.cs
@@ -0,0 +1,29 @@
+﻿//using Android.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class GeneralModel
+    {
+        public ObservableCollection<string> GenSettings { get; set; }
+     
+        public GeneralModel()
+        {
+            GenSettings = new ObservableCollection<string>
+            {
+                "Account > ",
+                "Find a Trainer! > ",
+                "Location Services > ",
+                "Notifications > ",
+                "Settings > ",
+                "Export Data > ",
+                "Logout > "
+            };
+        }
+    }
+}
