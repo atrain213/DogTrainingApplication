1fbb4c1ba4a0401635547b87abfea8964db1e0d3
Jadyn Ramos <jramos6@villanova.edu>
2023-11-28 12:49:21 -0500

    training page update

--- Pages/TrainingPage.xaml.cs
+++ Pages/TrainingPage.xaml.cs
@@ -6,11 +6,39 @@ public partial class TrainingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void StartSession_Clicked(object sender, EventArgs e)
+
+    {
+
+        await Shell.Current.GoToAsync($"/StartSession");
+
+    }
     private async void NewSession_Clicked(object sender, EventArgs e)
 
     {
 
         await Shell.Current.GoToAsync($"/NewSession");
+
+    }
+    private async void PlanSession_Clicked(object sender, EventArgs e)
+
+    {
+
+        await Shell.Current.GoToAsync($"/PlanSession");
+
+    }
+    private async void Tricks_Clicked(object sender, EventArgs e)
+
+    {
+
+        await Shell.Current.GoToAsync($"/Tricks");
+
+    }
+    private async void DetailSession_Clicked(object sender, EventArgs e)
+
+    {
+
+        await Shell.Current.GoToAsync($"/DetailSession");
 
     }
 }
