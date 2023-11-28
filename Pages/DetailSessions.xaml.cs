a1cf1d330bb901c6faf4ce80932e0b381df536e2
JT Tognucci <jtognucc@villanova.edu>
2023-11-28 13:56:08 -0500

    bio page

--- Pages/DetailSessions.xaml.cs
+++ Pages/DetailSessions.xaml.cs
@@ -11,5 +11,6 @@ public partial class DetailSessions : ContentPage
 	{
 		string CurrentSessionNotes = CurrentSessionNotesEntry.Text;
 		string NextSessionNotes = NextSessionNotesEntry.Text;
-		string CustomNotes = CustomNotesEntry;
+	//string CustomNotes = CustomNotesEntry;
+	}
 }
